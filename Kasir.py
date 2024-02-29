7fbcd410668dc4b450e36e0398ce24b57609c2ad
Naokturnal <72050624+nao0856@users.noreply.github.com>
2024-02-29 11:19:27 +0700

    Add files via upload

---
+++ Kasir.py
@@ -0,0 +1,12 @@
+rokok = 20000
+minyak = 25000
+gula = 11000
+saldo = 100000
+diskon = 10000
+kembalian = saldo - ((rokok + minyak + gula) - diskon)
+print("Rokok : ", rokok)
+print("Minyak : ", minyak)
+print("Gula : ", gula)
+print("Diskon", diskon)
+print("Saldo :", saldo)
+print("Kembalian ucup :",kembalian)
